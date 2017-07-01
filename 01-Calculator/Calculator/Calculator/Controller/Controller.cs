4c7bd7102882bf11decf547f767819df2264f93a
Michael Chen W <michael.cw02@hotmail.com>
2017-06-30 18:46:11 -0600

    Calculator Complete

---
+++ 01-Calculator/Calculator/Calculator/Controller/Controller.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calculator
+{
+    class Controller
+    {
+        private MainWindow View { set; get; }
+        enum Numbers        { ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE }
+        string Current      { get; set; }
+        string Operation    { get; set; }
+        Boolean hasOperation { get; set; }
+        double? Stored;
+
+        public Controller (MainWindow view)
+        {
+            View = view;
+            Current = "0";
+            Operation = "None";
+            Stored = null;
+            setTextBoxValue(Current);
+        }
+
+
+        public void setTextBoxValue(string value)
+        {
+            View.textBox.Text = value;
+        }
+
+        public void enteredValue(string value)
+        {
+            Numbers num;
+            if(Enum.TryParse(value, true, out num))
+            {
+                Current = (hasOperation) ? ((int)num).ToString() : Current + (int)num;
+                setTextBoxValue(Current);
+                return;
+            }
+            switch(value)
+            {
+                case "COMMA":
+                    Current += ".";
+                    setTextBoxValue(Current);
+                    break;
+                case "CLEAR_ENTRY":
+                    Current = "0";
+                    setTextBoxValue(Current);
+                    break;
+                case "CLEAR":
+                    Console.WriteLine("CLEAR");
+                    Current = "0";
+                    Stored = 0;
+                    Operation = "None";
+                    hasOperation = false;
+                    Stored = null;
+                    setTextBoxValue(Current);
+                    break;
+                case "PLUS":
+                case "MINUS":
+                case "MULTIPLIED":
+                case "DIVIDED":
+                    Operation = value;
+                    prepOperation(Double.Parse(Current));
+                    hasOperation = true;
+                    break;
+                case "EQUALS":
+                    double r = (Stored == null) ? doOperation(Stored ?? default(double), Double.Parse(Current), Operation) 
+                                                : doOperation(Double.Parse(Current), Double.Parse(Current), Operation);
+                    setTextBoxValue(r.ToString());
+                    Current = r.ToString();
+                    Stored = null;
+                    hasOperation = false;
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+
+        public void prepOperation(double current)
+        {
+            Stored = (Stored != null) ? doOperation(Stored ?? default(double), current, Operation) : current;
+            setTextBoxValue(Stored.ToString());
+        }
+
+        public double doOperation(double first, double second, string operation)
+        {
+            double result;
+            switch(operation)
+            {
+                case "PLUS":
+                    result = first + second;
+                    break;
+                case "MINUS":
+                    result = first - second;
+                    break;
+                case "MULTIPLIED":
+                    result = first * second;
+                    break;
+                case "DIVIDED":
+                    //exception of dividing by 0
+                    result = first / second;
+                    break;
+                default:
+                    Console.WriteLine("Operation not created yet");
+                    result = -1;
+                    break;
+            }
+            return result;
+        }
+
+    }
+
+}
--- 01-Calculator/Calculator/Calculator/MainWindow.xaml.cs
+++ 01-Calculator/Calculator/Calculator/MainWindow.xaml.cs
@@ -20,14 +20,53 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        Controller Control;
+
         public MainWindow()
         {
             InitializeComponent();
+            Control = new Controller(this);
         }
 
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
+
+        private void btnZero_Click          (object sender, RoutedEventArgs e)  { Control.enteredValue("ZERO"); }
+
+        private void btnOne_Click           (object sender, RoutedEventArgs e)  { Control.enteredValue("ONE"); }
+
+        private void btnTwo_Click           (object sender, RoutedEventArgs e)  { Control.enteredValue("TWO"); }
+
+        private void btnThree_Click         (object sender, RoutedEventArgs e)  { Control.enteredValue("THREE"); }
+
+        private void btnFour_Click          (object sender, RoutedEventArgs e)  { Control.enteredValue("FOUR"); }
+
+        private void btnFive_Click          (object sender, RoutedEventArgs e)  { Control.enteredValue("FIVE"); }
+
+        private void btnSix_Click           (object sender, RoutedEventArgs e)  { Control.enteredValue("SIX"); }
+
+        private void btnSeven_Click         (object sender, RoutedEventArgs e)  { Control.enteredValue("SEVEN"); }
+
+        private void btnEight_Click         (object sender, RoutedEventArgs e)  { Control.enteredValue("EIGHT"); }
+
+        private void btnNine_Click          (object sender, RoutedEventArgs e)  { Control.enteredValue("NINE"); }
+
+        private void btnComma_Click         (object sender, RoutedEventArgs e)  { Control.enteredValue("COMMA"); }
+
+        private void btnClearEntry_Click    (object sender, RoutedEventArgs e)  { Control.enteredValue("CLEAR_ENTRY"); }
+
+        private void btnClear_Click         (object sender, RoutedEventArgs e)  { Control.enteredValue("CLEAR"); }
+
+        private void btnPlus_Click          (object sender, RoutedEventArgs e)  { Control.enteredValue("PLUS"); }
+
+        private void btnMinus_Click         (object sender, RoutedEventArgs e)  { Control.enteredValue("MINUS"); }
+
+        private void btnMultBy_Click        (object sender, RoutedEventArgs e)  { Control.enteredValue("MULTIPLIED"); }
+
+        private void btnDivBy_Click         (object sender, RoutedEventArgs e)  { Control.enteredValue("DIVIDED"); }
+
+        private void btnEquals_Click        (object sender, RoutedEventArgs e)  { Control.enteredValue("EQUALS"); }
     }
 }
